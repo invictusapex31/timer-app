6777bbfd50f0c941c625370a6ff39fbc97f400d0
invictusapex31 <chhabrarajat850@gmail.com>
2025-09-22 07:49:15 +0530

    Add files via upload

---
+++ analytics.js
@@ -0,0 +1,7 @@
+// analytics.js
+(function() {
+  var script = document.createElement("script");
+  script.defer = true;
+  script.src = "/_vercel/insights/script.js";
+  document.head.appendChild(script);
+})();
